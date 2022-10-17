1bb43e25bd11379542d927bd366090b75fb125f1
HectasYoussef <84864527+HectasYoussef@users.noreply.github.com>
2022-10-17 21:14:03 +0100

    Seeding Blood & Nationalitie & Religion In School System Laravel

---
+++ app/Model/Type_Blood.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Type_Blood extends Model
+{
+    use HasFactory;
+    protected $fillable=['Name'];
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use Database\Seeders\BloodTableSeeder;
+use Database\Seeders\NationalitiesTableSeeder;
+use Database\Seeders\ReligionTableSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+     $this->call(BloodTableSeeder::class);
+     $this->call(NationalitiesTableSeeder::class);
+     $this->call(ReligionTableSeeder::class);
     }
 }
